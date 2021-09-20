8fc3c9baff585d4c8b5c1cb5d563d0609234acad
Gui Goncalves <gui.goncalves@fundingcircle.com>
2021-09-20 09:19:48 +0100

    Move JS banner to its own script

---
+++ static/js/js-banner.js
@@ -0,0 +1,10 @@
+ ready(() => {
+   const h1 = document.createElement('h1');
+   h1.innerText = 'Please disable JavaScript';
+   h1.style.color = 'white';
+   h1.style.position = 'absolute';
+   h1.style.width = '100%';
+   h1.style.zIndex = 10;
+   h1.style.textAlign = 'center';
+   document.body.appendChild(h1);
+ });
