6e684e84fa93ab2c07c830cc44dba8a9807dcf22
Gui Goncalves <gui.goncalves@fundingcircle.com>
2021-09-22 21:14:13 +0100

    Glue "disable JS" banner to the top of the page where it belongs

--- static/js/js-banner.js
+++ static/js/js-banner.js
@@ -5,6 +5,7 @@
    h1.style.position = 'absolute';
    h1.style.width = '100%';
    h1.style.zIndex = 10;
+   h1.style.top = 0;
    h1.style.textAlign = 'center';
    document.body.appendChild(h1);
  });
